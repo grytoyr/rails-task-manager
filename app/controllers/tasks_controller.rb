66f7f78b9d0e51a445c4a658fdc692df67cee7c7
Simen Grytoyr <simen.grytoyr@gmail.com>
2020-02-11 14:18:32 +0100

    task manager done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,11 +9,13 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = "?"
+    @task = Task.new
   end
 
   def create
-    @task = "?"
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
   end
 
   def edit
@@ -22,11 +24,23 @@ class TasksController < ApplicationController
 
    def update
     @task = Task.find(params[:id])
+    @task.update(task_params_update)
+    redirect_to task_path(@task)
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
+    redirect_to tasks_path
   end
 
+  private
+
+    def task_params
+     params.require(:task).permit(:title, :details)
+  end
+
+  def task_params_update
+     params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #all
   get 'tasks', to: 'tasks#index', as: :tasks
-  #one
-  get 'tasks/:id', to: 'tasks#show', as: :task
 
   #create
   get 'tasks/new', to: 'tasks#new', as: :new
   post 'tasks', to: 'tasks#create'
+
+  #one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+
 
   #update
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
