c649cb17fc94315b561dccb39fb26f3ab74cd334
Simen Grytoyr <simen.grytoyr@gmail.com>
2020-02-11 11:30:07 +0100

    controller build

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #all
+  get 'tasks', to: 'tasks#index', as: tasks
+  #one
+  get 'tasks/:id', to 'tasks#show', as: task
+
+  #create
+
+  #update
+
+  #delete
 end
